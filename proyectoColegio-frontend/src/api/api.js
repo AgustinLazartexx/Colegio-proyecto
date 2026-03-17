cb0512b4897ae120ebe356ace3a576b30732c6f4
AgustinLazartexx <agustinlazarte412@gmail.com>
2026-03-17 14:20:59 -0300

    conecfront1.0

--- proyectoColegio-frontend/src/api/api.js
+++ proyectoColegio-frontend/src/api/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // 1. Configuración inicial
 const api = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/api`
+  baseURL: "https://colegio-proyecto-backend.onrender.com/api"
 });
 
 // 2. Interceptor de Token
